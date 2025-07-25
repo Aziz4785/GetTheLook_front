1620af5ea0bc6f8860b4c3964d4aa6ddaaf0b73e
Azizk <azizkanoun3@gmail.com>
2025-07-25 12:36:19 +0200

    version 5

--- app.config.js
+++ app.config.js
@@ -32,7 +32,7 @@ export default ({ config }) => ({
     ...config,
     "name": getAppName(),
     "slug": "getTheLook_front",
-    "version": "1.0.4",
+    "version": "1.0.5",
     "orientation": "portrait",
     "icon": "./assets/images/icon.png",
     "scheme": "getthelookfront",
